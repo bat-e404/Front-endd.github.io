e252c7c6298e9e73038d097dc57cb82bc2b4198c
Bryan Aguirre Tudon <bryan_tudon@outlook.com>
2022-11-14 14:32:38 -0600

    Primera Actualizacion

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import AlertOferta from './componet/AlertOferta';
+import NavBar from './componet/NavBar';
+
+function App() {
+  return (
+    <>
+      <AlertOferta/>
+      <NavBar/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componet/AlertOferta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AlertOferta() {
+  return (
+    <div class="conatiner pt-2" id="alertaOferta">
+        <div class="row text-center">
+            <p>Super Oferta - <strong>Envio Gratis</strong> en pedidos superiores de $600.00 MX</p>
+        </div>
+    </div>
+  )
+}
